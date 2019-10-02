88f03981c6dbf8d36d05015d443b90b0f855075f
Jason Gao <jason@Jasons-MacBook-Pro.local>
2019-10-02 14:11:16 -0700

    refactor structure and add reducer redux store

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+
+
+export const CONSTANTS = {
+    LOG_IN: 'LOG_IN'
+}
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+import rootReducer from '../reducers'
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunkMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+  // compose(
+  //   applyMiddleware(thunkMiddleware)
+  // )
+)
+
+export default store
