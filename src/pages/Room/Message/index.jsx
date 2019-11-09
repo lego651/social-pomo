89d1cab664d69a86cedd1dd65b9ff1fa452decfc
Jason Gao <jason@Jasons-MacBook-Pro.local>
2019-11-09 15:35:28 -0800

    add chatroom message UI

---
+++ src/pages/Room/Message/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { addMessage } from '../../../actions';
+import './style.scss';
+
+import avatar from '../../../assets/img/default_avatar.jpg';
+
+const Message = ({ item, curHandle }) => {
+  console.log(curHandle);
+  const isOwner = (item.userHandle === curHandle)
+  return(
+    <div>
+      <img className={"userImg" + (isOwner ? " isOwner" : "")} src={avatar} alt="avatar" />
+      <div className={"message-container" + (isOwner ? " isOwner" : "")}>
+        <div className="message-wrap">
+          <div className="message-sender">
+            <b> <i>{item.userHandle} </i></b>
+          </div>
+          <div className="message-content">
+            <p> {item.content} </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Message
