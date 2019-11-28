99301c4896aae27acb61cc394b218418d1a3c35a
pomogao <legogao651@gmail.com>
2019-11-28 15:41:27 -0800

    add dropdown menu in navbartop component

--- src/components/NavbarTop/index.jsx
+++ src/components/NavbarTop/index.jsx
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Dropdown, DropdownButton, Row } from 'react-bootstrap';
+import { DropdownMenu, MenuItem } from 'react-bootstrap-dropdown-menu';
 
 import './style.scss';
-import { addTodo } from '../../actions';
-// import avatar from '../../assets/img/default_avatar.jpg';
+import { addTodo,logoutUser } from '../../actions';
+import avatar from '../../assets/img/default_avatar.jpg';
 // import { addMessage } from '../../actions';
 
 class NavbarTop extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      content: ''
+      content: '',
+      showMenu: false,
+    }
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+  showMenu(event) {
+    event.preventDefault();
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+  closeMenu(event) {
+    if (!this.dropdownMenu.contains(event.target)) {
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });
     }
   }
+  handleLogout = () => {
+    this.props.logoutUser();
+  }
+
   render(){
-    const { avatar } = this.props.user.profile;
     return(
       <div className="navbar-container">
         <Container>
@@ -23,8 +43,32 @@ class NavbarTop extends Component {
             <Navbar.Brand href="/overview"> Social Pomo </Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                <img src={avatar} alt="avatar" />
+              <Navbar.Text className="dropdown">
+                <img
+                  onClick={this.showMenu}
+                  src={avatar}
+                  alt="avatar"
+                />
+                {
+                  this.state.showMenu
+                     ? (
+                       <div
+
+                         ref={(e) => {
+                           this.dropdownMenu = e;
+                         }}
+                       >
+                       <div className="dropdown-content">
+                           <a onClick={this.handleLogout}>Log out</a>
+                           <a href="#">Link 2</a>
+                           <a href="#">Link 3</a>
+                         </div>
+                       </div>
+                     )
+                     : (
+                       null
+                     )
+                }
               </Navbar.Text>
             </Navbar.Collapse>
           </Navbar>
@@ -41,5 +85,5 @@ const mapStateToProps = (state) => ({
 
 export default connect(
   mapStateToProps,
-  { addTodo }
+  { addTodo, logoutUser }
 )(NavbarTop);
