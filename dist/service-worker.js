10c7cee9110b90051e638d7889eab205ddcb5040
pomogao <legogao651@gmail.com>
2019-12-20 21:30:52 -0800

    new deployment

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6db1897d4165aadf31fe70ec72359a3.js"
+  "/precache-manifest.f279ca9304694a709076577de1f1be64.js"
 );
 
 self.addEventListener('message', (event) => {
