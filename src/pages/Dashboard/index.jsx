c176f3ab00abe2de9ee1651a5dbf21ca0e6a2f1d
catherine <cathyweiyiliu@gmail.com>
2020-04-26 15:31:40 -0700

    fixbug#38

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -23,8 +23,8 @@ import { logoutUser, getWeeklyPomo } from "../../actions";
 import "./style.scss";
 
 // utils
-import { convertDateToSeq } from '../../utils/util';
-var _ = require('lodash');
+import { convertDateToSeq } from "../../utils/util";
+var _ = require("lodash");
 
 class Dashboard extends Component {
   constructor(props) {
@@ -97,9 +97,9 @@ class Dashboard extends Component {
   }
 
   groupByProject(pomos) {
-    return _.groupBy(pomos, 'project');
+    return _.groupBy(pomos, "project");
   }
-  
+
   buildDataSet(pomos) {
     const colors = ["#FAEBCC", "#D6E9C6", "#EBCCD1"];
     let c = 0;
@@ -108,28 +108,28 @@ class Dashboard extends Component {
     const m = dateObj.getMonth();
     const d = dateObj.getDate();
     const seq = convertDateToSeq(y, m + 1, d);
-    const week = [seq-6,seq-5,seq-4,seq-3,seq-2,seq-1,seq];
+    const week = [seq - 6, seq - 5, seq - 4, seq - 3, seq - 2, seq - 1, seq];
     const groupedData = this.groupByProject(pomos);
     const datasets = [];
-    
+
     for (let [project, pomos] of Object.entries(groupedData)) {
-      const data = [0,0,0,0,0,0,0];
+      const data = [0, 0, 0, 0, 0, 0, 0];
       const set = {};
-        
+
       pomos.forEach((pomo) => {
-        for(let i = 0; i < week.length; i++) {
-          if(pomo.seq === week[i]) {
+        for (let i = 0; i < week.length; i++) {
+          if (pomo.seq === week[i]) {
             data[i] += Math.round((pomo.time / 3600) * 100) / 100;
           }
-        } 
+        }
       });
-      
+
       set.label = project;
       set.backgroundColor = colors[c++];
       set.data = data;
       datasets.push(set);
     }
-  
+
     return datasets;
   }
 
@@ -137,10 +137,28 @@ class Dashboard extends Component {
     const pomos = this.props.pomo.weekly_pomo;
     const datasets = this.buildDataSet(pomos);
     // const labels = [110,111,112,113,114,115,116];
-    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];     
-    const d = new Date();     
-    const labels = [days[d.getDay() - 6],days[d.getDay() - 5],days[d.getDay() - 4],days[d.getDay() - 3],days[d.getDay() - 2],days[d.getDay() - 1],days[d.getDay()]];   
-    
+
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const d = new Date();
+    const labels = [
+      days[(d.getDay() + 1) % 7],
+      days[(d.getDay() + 2) % 7],
+      days[(d.getDay() + 3) % 7],
+      days[(d.getDay() + 4) % 7],
+      days[(d.getDay() + 5) % 7],
+      days[(d.getDay() + 6) % 7],
+      days[d.getDay()],
+    ];
+    // const labels = [days[d.getDay() - 6],days[d.getDay() - 5],days[d.getDay() - 4],days[d.getDay() - 3],days[d.getDay() - 2],days[d.getDay() - 1],days[d.getDay()]];
+
     if (pomos.length > 0) {
       return (
         <div className="chart-container">
