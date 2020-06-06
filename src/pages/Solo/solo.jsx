a2978ef354739e24d6a5e2f3f672d7e528a5bf03
JasonGao99 <jasongaoxiaofeng@gmail.com>
2020-06-06 11:14:49 -0700

    [FEAT] Add solo timer

--- src/pages/Solo/solo.jsx
+++ src/pages/Solo/solo.jsx
@@ -6,15 +6,48 @@ import { Container, Row, Col } from 'react-bootstrap';
 import NavbarTop from '../../components/NavbarTop';
 import NavLeft from '../../components/NavLeft';
 
+// Utils
+import { parseTime } from "../../utils/util.js";
+
+// Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle, faPauseCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
 // Styles
 import './solo.scss';
 
 class Solo extends Component {
   constructor(props) {
     super(props);
+    this.interval = null;
     this.state = {
-      value: 25
+      value: 5,
+      on: false,
     }
+  }
+
+  onStart = () => {
+    this.setState({on: true});
+    this.interval = setInterval(
+      () => {
+        if(this.state.value > 0) {
+          this.setState(({value}) => ({value: value - 1}))
+        } else {
+          this.setState({on: false, value:25*60});
+          clearInterval(this.interval);
+        }
+      }
+    , 1000);
+  }
+
+  onPause = () => {
+    this.setState({on: false});
+    clearInterval(this.interval);
+  }
+
+  onReset = () => {
+    this.setState({on: false, value:25*60});
+    clearInterval(this.interval);
   }
 
   buildRangeInput = () => {
@@ -25,16 +58,26 @@ class Solo extends Component {
           name="Set Timer"
           maxValue={60}
           minValue={0}
-          value={this.state.value}
-          onChange={value => this.setState({ value })} />
+          value={Math.round(this.state.value / 60)}
+          onChange={value => this.setState({ value: value * 60})} />
       </>
     )
   }
 
-  buildPomoTimer = () => {
+  buildTimer = () => {
     return (
-      <div className="timer-container">
-        {this.buildRangeInput()}
+      <div className="timer">
+        {parseTime(this.state.value)}
+      </div>
+    )
+  }
+
+  buildButtonGroup = () => {
+    return (
+      <div className="buttons">
+        {!this.state.on && <span onClick={this.onStart}><FontAwesomeIcon icon={faPlayCircle} /></span>}
+        {this.state.on && <span onClick={this.onPause}><FontAwesomeIcon icon={faPauseCircle} /></span>}
+        <span onClick={this.onReset}><FontAwesomeIcon icon={faTimesCircle} /></span>
       </div>
     )
   }
@@ -52,7 +95,11 @@ class Solo extends Component {
               <div className="solo-header">
                 <h3> Solo </h3>
               </div>
-              {this.buildPomoTimer()}
+              <div className="timer-container">
+                {this.buildRangeInput()}
+                {this.buildTimer()}
+                {this.buildButtonGroup()}
+              </div>
             </Col>
           </Row>
         </Container>   
