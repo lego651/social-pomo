3081821dc82e0976530a76017d70c0ff5e7e38c7
JasonGao99 <jasongaoxiaofeng@gmail.com>
2020-07-14 18:22:34 -0700

    [FEAT]: Refactor NavbarTop component

--- src/components/NavbarTop/index.jsx
+++ src/components/NavbarTop/index.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+// Components 
 import { Container, Navbar } from 'react-bootstrap';
+
+// Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
+// Styles
 import './style.scss';
+
+// Actions
 import { addTodo,logoutUser } from '../../actions';
+
+// Images
 import default_img from '../../assets/img/avatar.svg';
 
 class NavbarTop extends Component {
@@ -18,12 +27,14 @@ class NavbarTop extends Component {
     this.showMenu = this.showMenu.bind(this);
     this.closeMenu = this.closeMenu.bind(this);
   }
+
   showMenu(event) {
     event.preventDefault();
     this.setState({ showMenu: true }, () => {
       document.addEventListener('click', this.closeMenu);
     });
   }
+
   closeMenu(event) {
     if (!this.dropdownMenu.contains(event.target)) {
       this.setState({ showMenu: false }, () => {
@@ -31,53 +42,56 @@ class NavbarTop extends Component {
       });
     }
   }
+
   handleLogout = () => {
     this.props.logoutUser();
   }
 
+  buildAvatar = () => {
+    const { avatar } = this.props.user.profile;
+    return (
+      <div className="dropdown-wrapper" onClick={this.showMenu}>
+        <img src={avatar ? avatar : default_img} alt="avatar" />
+        <span><FontAwesomeIcon icon={faCaretDown} /></span>
+      </div>
+    )
+  }
+
+  buildDropdown = () => {
+    return this.state.showMenu && (
+      <div ref={(e) => { this.dropdownMenu = e; }}>
+        <div className="dropdown-content">
+          <a href="/dashboard"> Dashboard </a>
+          <a href="/room"> Room </a>
+          <a href="/match"> Match </a>
+          <a href="/account"> Account </a>
+          <a href="/" onClick={this.handleLogout}> Sign out </a>
+        </div>
+      </div>
+    )
+  }
+
+  buildAvatarAndDropdown = () => {
+    return (
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text className="username">
+          { this.props.user.profile.nickName }
+        </Navbar.Text>
+        <Navbar.Text className="dropdown">
+          {this.buildAvatar()}
+          {this.buildDropdown()}
+        </Navbar.Text>
+      </Navbar.Collapse>
+    )
+  }
+
   render(){
-    const { avatar, nickName } = this.props.user.profile;
     return(
       <div className="navbar-container">
         <Container>
           <Navbar>
             <Navbar.Brand href="/dashboard"> Pomopal </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text className="username">
-                { nickName }
-              </Navbar.Text>
-              <Navbar.Text className="dropdown">
-                <div className="dropdown-wrapper" onClick={this.showMenu}>
-                  <img
-                    src={avatar ? avatar : default_img}
-                    alt="avatar"
-                  />
-                  <span> <FontAwesomeIcon icon={faCaretDown} /></span>
-                </div>
-                {
-                  this.state.showMenu
-                     ? (
-                       <div
-                         ref={(e) => {
-                           this.dropdownMenu = e;
-                         }}
-                       >
-                       <div className="dropdown-content">
-                           <a href="/dashboard"> Dashboard </a>
-                           <a href="/room"> Room </a>
-                           <a href="/match"> Match </a>
-                           <a href="/account"> Account </a>
-                           <a href="/" onClick={this.handleLogout}> Sign out </a>
-                         </div>
-                       </div>
-                     )
-                     : (
-                       null
-                     )
-                }
-              </Navbar.Text>
-            </Navbar.Collapse>
+            {this.buildAvatarAndDropdown()}
           </Navbar>
         </Container>
       </div>
