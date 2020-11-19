060fc7c53e393e2260bddd348e4bed52bbebe183
JasonGao99 <jasongaoxiaofeng@gmail.com>
2020-11-18 17:35:19 -0800

    update gitignore

--- backend/config/firebase.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyA-EVP5tjgcdoSp6l2KG7azuoq3jsRzl8o",
-    authDomain: "pomopal-dev.firebaseapp.com",
-    databaseURL: "https://pomopal-dev.firebaseio.com",
-    projectId: "pomopal-dev",
-    storageBucket: "pomopal-dev.appspot.com",
-    messagingSenderId: "504847911416",
-  };
-  
